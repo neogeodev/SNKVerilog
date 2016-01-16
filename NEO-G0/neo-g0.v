c91f64bddc6fdf5281198986ded9dcdb6acf5931
Furrtek <furrtek@gmail.com>
2016-01-16 05:39:44 +0100

    Created NEO-G0 ISE project and pin assignations

--- NEO-G0/neo-g0.v
+++ NEO-G0/neo-g0.v
@@ -1,19 +1,41 @@
-// What happens when both /CE are low ? Chip auto-destructs ?
+// SNK NEO-G0 chip logic
+// Part of SNKVerilog
+// ©2016 furrtek
+
+// Nothing is tested
+
+module NEOG0 (
+	inout [15:0] A,
+	inout [15:0] B,
+	inout [15:0] C,
+	input nCEA,
+	input nCEB,
+	input SELECTL,
+	input SELECTU,
+	output ORO,
+	input ANDI0,
+	input ANDI1,
+	output ANDO
+);
+
+// What happens when both /CE are low on the real chip ? Is there a priority, or does the chip auto-destructs ?
 
 // A0~7 <= B0~7 or C0~7 (B has priority)
-assign {18,17,16,15,5,4,3,2} = 40 ? 39 ? 51 ? 8'bzzzzzzzz : {14,13,12,11,9,8,7,6} : {24,23,22,21,1,64,63,62} : 8'bzzzzzzzz;
+assign A[7:0] = SELECTL ? nCEA ? nCEB ? 8'bzzzzzzzz : C[7:0] : B[7:0] : 8'bzzzzzzzz;
 // A8~15 <= B8~15 or C8~15 (B has priority)
-assign {50,49,48,47,34,33,32,31} = 52 ? 39 ? 51 ? 8'bzzzzzzzz : {46,45,44,43,30,29,28,27} : {56,55,54,53,38,37,36,35} : 8'bzzzzzzzz;
+assign A[15:8] = SELECTU ? nCEA ? nCEB ? 8'bzzzzzzzz : C[15:8] : B[15:8] : 8'bzzzzzzzz;
 
 // B0~7 <= A0~7
-assign {24,23,22,21,1,64,63,62} = 39 ? 8'bzzzzzzzz : 40 ? 8'bzzzzzzzz : {18,17,16,15,5,4,3,2};
+assign B[7:0] = nCEA ? 8'bzzzzzzzz : SELECTL ? 8'bzzzzzzzz : A[7:0];
 // B8~15 <= A8~15
-assign {56,55,54,53,38,37,36,35} = 39 ? 8'bzzzzzzzz : 52 ? 8'bzzzzzzzz : {50,49,48,47,34,33,32,31};
+assign B[15:8] = nCEA ? 8'bzzzzzzzz : SELECTU ? 8'bzzzzzzzz : A[15:8];
 
 // C0~7 <= A0~7
-assign {14,13,12,11,9,8,7,6} = 51 ? 8'bzzzzzzzz : 40 ? 8'bzzzzzzzz : {18,17,16,15,5,4,3,2};
+assign C[7:0] = nCEB ? 8'bzzzzzzzz : SELECTL ? 8'bzzzzzzzz : A[7:0];
 // C8~15 <= A8~15
-assign {46,45,44,43,30,29,28,27} = 51 ? 8'bzzzzzzzz : 52 ? 8'bzzzzzzzz : {50,49,48,47,34,33,32,31};
+assign C[15:8] = nCEB ? 8'bzzzzzzzz : SELECTU ? 8'bzzzzzzzz : A[15:8];
 
-assign 41 = 40 | 39;
-assign 59 = 60 & 61;
+assign ORO = SELECTL | nCEA;
+assign ANDO = ANDI0 & ANDI1;
+
+endmodule
